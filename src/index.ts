080a67f5b02de903ba9a588bf2af9240b4d9166e
jmnuf <jose.m.nufio.0@gmail.com>
2025-04-20 17:15:06 +0200

    [feat] Add functions for mapping errors or values

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,38 @@
-type OkResult<T> = { ok: true, value: T; };
-type ErrResult = { ok: false, error: Error; };
+
+interface IResult<Ok extends boolean, Value> {
+  ok: Ok;
+  mapVal<R>(fn: (value: Value) => R): Result<R>;
+  mapErr<R extends Error>(fn: (error: Error) => R): Result<Value>;
+};
+interface OkResult<T> extends IResult<true, T> {
+  value: T;
+};
+interface ErrResult extends IResult<false, never> {
+  error: Error;
+};
 export type Result<T> = OkResult<T> | ErrResult;
 
-
 function Ok<T>(value: T): Result<T> {
-  return { ok: true, value };
+  const ok: OkResult<T> = {
+    ok: true,
+    value,
+  } as any;
+  ok.mapVal = mapVal.bind(null, ok as any) as any;
+  ok.mapErr = mapErr.bind(null, ok as any) as any;
+  return ok;
 }
 
-function Err(err: string | Error): ErrResult {
-  if (typeof err === "string") {
-    err = new Error(err);
+function Err(error: string | Error): ErrResult {
+  if (typeof error === "string") {
+    error = new Error(error);
   }
-  return { ok: false, error: err };
+  const e: ErrResult = {
+    ok: false,
+    error,
+  } as any;
+  e.mapVal = mapVal.bind(null, e as any) as any;
+  e.mapErr = mapErr.bind(null, e as any) as any;
+  return e;
 }
 
 function syncCall<T>(fn: () => T): Result<T> {
@@ -30,6 +51,26 @@ async function asyncCall<T>(fn: (() => Promise<T>) | Promise<T>): Promise<Result
   } catch (error) {
     return Err(error as Error);
   }
+}
+
+function mapVal<T, R>(fromRes: Result<T>, fn: (value: T) => R): Result<R> {
+  if (!fromRes.ok) {
+    return fromRes;
+  }
+  try {
+    const value = fn(fromRes.value);
+    return Ok(value);
+  } catch (e) {
+    return Err(e as Error);
+  }
+}
+
+function mapErr<T, E extends Error>(fromRes: Result<T>, fn: (error: Error) => E): Result<T> {
+  if (fromRes.ok) {
+    return fromRes;
+  }
+  const err = fn(fromRes.error);
+  return Err(err);
 }
 
 export const Res = Object.freeze({
