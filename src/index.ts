d8d266068b4284566a369e155bec4cb561ab43ac
jmnuf <jose.m.nufio.0@gmail.com>
2025-04-20 16:48:11 +0200

    [chore] format

--- src/index.ts
+++ src/index.ts
@@ -4,37 +4,37 @@ export type Result<T> = OkResult<T> | ErrResult;
 
 
 function Ok<T>(value: T): Result<T> {
-	return { ok: true, value };
+  return { ok: true, value };
 }
 
 function Err(err: string | Error): ErrResult {
-	if (typeof err === "string") {
-		err = new Error(err);
-	}
-	return { ok: false, error: err };
+  if (typeof err === "string") {
+    err = new Error(err);
+  }
+  return { ok: false, error: err };
 }
 
 function syncCall<T>(fn: () => T): Result<T> {
-	try {
-		const value = fn();
-		return Ok(value);
-	} catch (error) {
-		return Err(error as Error);
-	}
+  try {
+    const value = fn();
+    return Ok(value);
+  } catch (error) {
+    return Err(error as Error);
+  }
 }
 
 async function asyncCall<T>(fn: (() => Promise<T>) | Promise<T>): Promise<Result<T>> {
-	try {
-		const value = await (typeof fn === "function" ? fn() : fn);
-		return Ok(value);
-	} catch (error) {
-		return Err(error as Error);
-	}
+  try {
+    const value = await (typeof fn === "function" ? fn() : fn);
+    return Ok(value);
+  } catch (error) {
+    return Err(error as Error);
+  }
 }
 
 export const Res = Object.freeze({
-	Ok,
-	Err,
-	syncCall,
-	asyncCall,
+  Ok,
+  Err,
+  syncCall,
+  asyncCall,
 });
